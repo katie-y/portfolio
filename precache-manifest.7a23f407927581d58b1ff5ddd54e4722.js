914e7ba6c9e8a0d6cdcb480f51e9a3faa29727da
katie-y <katie_york@hotmail.co.uk>
2020-04-06 13:46:58 +0100

    Updates

---
+++ precache-manifest.7a23f407927581d58b1ff5ddd54e4722.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c8003330413e4ed7ef52e93a6fa807",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "78822a13e11272908e36",
+    "url": "/portfolio/static/css/main.8043ec82.chunk.css"
+  },
+  {
+    "revision": "2eab3736aa5e67d18f70",
+    "url": "/portfolio/static/js/2.b5ce0dc0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.b5ce0dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78822a13e11272908e36",
+    "url": "/portfolio/static/js/main.1e852f0c.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "c9d4a4514c6fa6cd780f1eb8418cd9f1",
+    "url": "/portfolio/static/media/2dicegame.c9d4a451.png"
+  },
+  {
+    "revision": "054150055ad77c974e56879616a1a935",
+    "url": "/portfolio/static/media/Untitled.05415005.png"
+  },
+  {
+    "revision": "6b908788d53db571b893fef632a1b640",
+    "url": "/portfolio/static/media/left.6b908788.png"
+  },
+  {
+    "revision": "857f62ef2965c0304ff3f0521355ae8c",
+    "url": "/portfolio/static/media/nasa.857f62ef.png"
+  }
+]);
